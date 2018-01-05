1326f3f0f25987c3f37acac6430692f2c388638c
Kentaro Hayashi <kenhys@gmail.com>
2018-01-05 18:23:47 +0900

    Support composing mode for Firefox

--- js/contents.js
+++ js/contents.js
@@ -1,3 +1,33 @@
+function taaaaaaan(current, isEnter) {
+    var prefix = isEnter ? 'tan' : 'kata';
+    var size = isEnter ? rand(80,200) : rand(10,200);
+    var caretPosition = Measurement.caretPos(current);
+    var imgUrl = chrome.extension.getURL('images/' + prefix + '_' + rand(1,4) + '.svg');
+    var $img = $('<img width="' + size + '">');
+    $img.attr('src', imgUrl);
+    $img.css({
+        'position' : 'absolute',
+        'top' : caretPosition.top + rand(-10,10),
+        'left' : caretPosition.left + rand(-10,10),
+        'zIndex' : 99999
+    });
+    $('body').append($img);
+    $img.animate({
+        'top' : caretPosition.top + rand(-40,40),
+        'left' : caretPosition.left + rand(-40,40),
+        'width' : size + (isEnter ? rand(30,50) : rand(10,20)),
+        'opacity' : 0
+    },
+    500,
+    function(){
+	$img.remove();
+    })
+}
+
+function rand(min,max) {
+    return Math.floor(Math.random()*(max-min)+min);
+}
+
 document.onkeydown = function(e) {
     var current = document.activeElement;
     
@@ -5,32 +35,18 @@ document.onkeydown = function(e) {
 
     if (current.type === 'textarea' || current.type === 'text' || current.type === 'search') {
         var isEnter = e.key === 'Enter';
-        var prefix = isEnter ? 'tan' : 'kata';
-        var size = isEnter ? rand(80,200) : rand(10,200);
-        var caretPosition = Measurement.caretPos(current);
-        var imgUrl = chrome.extension.getURL('images/' + prefix + '_' + rand(1,4) + '.svg');
-        var $img = $('<img width="' + size + '">');
-        $img.attr('src', imgUrl);
-        $img.css({
-            'position' : 'absolute',
-            'top' : caretPosition.top + rand(-10,10),
-            'left' : caretPosition.left + rand(-10,10),
-            'zIndex' : 99999
-        });
-        $('body').append($img);
-        $img.animate({
-            'top' : caretPosition.top + rand(-40,40),
-            'left' : caretPosition.left + rand(-40,40),
-            'width' : size + (isEnter ? rand(30,50) : rand(10,20)),
-            'opacity' : 0
-        },
-        500,
-        function(){
-            $img.remove();
-        })
-    }
-
-    function rand(min,max) {
-        return Math.floor(Math.random()*(max-min)+min);
+        taaaaaaan(current, isEnter);
     }
 }
+
+document.addEventListener("input", (event) => {
+    if (!event.isComposing) {
+	return;
+    }
+    var current = document.activeElement;
+
+    if (current.type === 'textarea' || current.type === 'text' || current.type === 'search') {
+        var isEnter = false;
+        taaaaaaan(current, isEnter);
+    }
+});
