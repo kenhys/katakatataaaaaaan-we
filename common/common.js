6b221554ce59b9f74621db3af816a3cf4dd3b14e
Kentaro Hayashi <kenhys@gmail.com>
2018-01-08 00:52:07 +0900

    Add options interface

---
+++ common/common.js
@@ -0,0 +1,33 @@
+/*
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
+var configs;
+
+function log(aMessage, ...aArgs)
+{
+	if (!configs || !configs.logging)
+		return;
+
+	console.log('katakatataaaaaaan-we: ' + aMessage, ...aArgs);
+}
+
+function debug(aMessage, ...aArgs)
+{
+	if (!configs || !configs.debug)
+		return;
+
+	log('[DEBUG] ' + aMessage, ...aArgs);
+}
+
+configs = new Configs({
+	enterKeyTaaaaaaanMinSize  : 80,
+	enterKeyTaaaaaaanMaxSize  : 100,
+	normalKeyKataKataMinSize  : 10,
+	normalKeyKataKataMaxSize  : 20,
+	animateVerticalPosition   : 40,
+	animateHorizontalPosition : 40,
+	debug                     : true
+});
